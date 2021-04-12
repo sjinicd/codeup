5331dee6a3e0b31ccc465c448bc652f95170026d
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-12 13:51:30 +0900

    Update 1023.java

--- 1021/1023.java
+++ 1021/1023.java
@@ -12,3 +12,14 @@ public class Main{
     System.out.println(list[1]);
   }
 }
+/*
+import java.util.Scanner;
+
+public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  String a = sc.next();
+  String[] A = a.split("\\.");
+  System.out.printf("%s\n%s",A[0],A[1]);
+  }
+ }
+*/
