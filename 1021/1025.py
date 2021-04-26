b5574ebf4a6702ec224fc5bf5e21386f60a5eccf
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-26 14:12:31 +0900

    Create 1025.py

---
+++ 1021/1025.py
@@ -0,0 +1,7 @@
+n = input()
+
+print("["+str(int(n[0])*10000+"]")
+print("["+str(int(n[1])*1000+"]")
+print("["+str(int(n[2])*100+"]")
+print("["+str(int(n[3])*10+"]")
+print("["+str(int(n[4])*1+"]")
