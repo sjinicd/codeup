003a518ebe1fad12f434bf5917b6d69801886cfc
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-27 20:16:37 +0900

    Create 1035.java

---
+++ 1031/1035.java
@@ -0,0 +1,14 @@
+//8진수로 바꾸어 출력한다.
+
+import java.util.Scanner;
+
+public class Main{
+  public class void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    
+    String n = sc.nextLine();
+    int num = Integer.parseInt(n,16);
+    System.out.println(Integer,toOctalString(num));
+  }
+}
