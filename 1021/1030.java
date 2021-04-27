b1663b91655b5d39129e8896b6e97d7e635b9a12
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-27 18:30:22 +0900

    Create 1030.java

---
+++ 1021/1030.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String arg[]){
+    
+    Scanner sc = sc.next();
+    
+    long n = sc.nextLong();
+    System.out.println(n);
+  }
+}
+
