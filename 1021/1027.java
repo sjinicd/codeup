ea65703377597a5ac2d3264471448eec1d26d248
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-27 18:04:23 +0900

    Create 1027.java

---
+++ 1021/1027.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    s= s.replace('.','-');
+    System.out.println(list[2]+"-"+list[1]+"-"+list[0]);
+  }
+}
+
