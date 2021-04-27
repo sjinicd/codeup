a2e242f5318e69885f3a550efaf3422eff91bc8f
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-27 20:17:34 +0900

    Update 1035.java

--- 1031/1035.java
+++ 1031/1035.java
@@ -8,7 +8,10 @@ public class Main{
     Scanner sc = new Scanner(System.in);
     
     String n = sc.nextLine();
+    //한문장 출력
     int num = Integer.parseInt(n,16);
+    //int n 값 16진수로 형변환
     System.out.println(Integer,toOctalString(num));
+    // toOtalString 메소드로 num값 String으로 형변환
   }
 }
