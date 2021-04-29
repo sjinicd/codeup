ee0100bbe6d03407ff3b10117711bb516c4fe35d
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-29 09:20:37 +0900

    Create 1037.java

---
+++ 1031/1037.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+  Scanner sc = new Scanner(System.in);
+  
+  int n = sc.nextInt();
+  
+  char c = (char)n;
+  
+  System.out.println(c);
+  
+  }
+}
+
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class codeUp1037{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    char c = (char) Integer.parseInt(br.readLine());
+    System.out.println(c);
+    
+
+*/
