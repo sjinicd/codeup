68d6ecf491566f6ed063dd6265b981067baa3471
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-29 09:04:58 +0900

    Create 1036.java

---
+++ 1036.java
@@ -0,0 +1,17 @@
+//아스키코드 값을 10진수로 출력한다.
+
+import java.util.Scanner;
+
+public class Main{
+  public static void main(Stringp[] args){
+    Scanner sc = sc.next(System.in);
+    
+    char c = sc.nextLine().charAt(0);
+    
+    int n = (int)c;
+    
+    System.out.println(n);
+  }
+}
+    
+    
