55d174cdec24eeefb569730c0cacda786ae65bb2
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-05-04 15:53:02 +0900

    Create 1037.py

---
+++ 1031/1037.py
@@ -0,0 +1,13 @@
+num = int(intput());
+print(chr(num));
+#전달받은 정수(integer, 0 ~ 255)를 ASCII character로 변환한다. 
+except Exception as e:
+  print(e);
+  
+  
+  
+# a=input()
+# n=int(a)
+# c=chr(n)
+# print(c)
+  
